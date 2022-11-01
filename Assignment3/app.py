734efc14ad4e800b8f2afcf386774413639cd6a5
timtheteh <76463517+timtheteh@users.noreply.github.com>
2022-11-01 00:04:42 -0700

    updated main.py

--- Assignment3/app.py
+++ Assignment3/app.py
@@ -151,11 +151,12 @@ class Assignment3VPN:
                 # Checking if the received message is part of your protocol
                 # TODO: MODIFY THE INPUT ARGUMENTS AND LOGIC IF NECESSARY
                 if self.prtcl.IsMessagePartOfProtocol(cipher_text):
+                    # print(cipher_text)
                     # Disabling the button to prevent repeated clicks
                     self.secureButton["state"] = "disabled"
                     # Processing the protocol message
-                    self.prtcl.ProcessReceivedProtocolMessage(cipher_text)
-
+                    self.prtcl.ProcessReceivedProtocolMessage(cipher_text, self.sharedSecret.get())
+                    # print(msg)
                 # Otherwise, decrypting and showing the messaage
                 else:
                     plain_text = self.prtcl.DecryptAndVerifyMessage(cipher_text)
@@ -179,7 +180,7 @@ class Assignment3VPN:
         self.secureButton["state"] = "disabled"
 
         # TODO: THIS IS WHERE YOU SHOULD IMPLEMENT THE START OF YOUR MUTUAL AUTHENTICATION AND KEY ESTABLISHMENT PROTOCOL, MODIFY AS YOU SEEM FIT
-        init_message = self.prtcl.GetProtocolInitiationMessage()
+        init_message = self.prtcl.GetProtocolInitiationMessage(self.mode.get())
         self._SendMessage(init_message)
 
 
